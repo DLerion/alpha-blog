6b5741c1a4eb5af47f3345e4ae1b891a167f8ee7
DLerion <daniel.leon@erion.mx>
2022-11-11 14:04:47 -0600

    Create destroy button and method

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
-
+  resources :articles
   # Defines the root path route ("/")
   # root "articles#index"
 end
