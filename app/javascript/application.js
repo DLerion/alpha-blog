1c14a827e1a764115fd259b579b353a2673058fe
DLerion <daniel.leon@erion.mx>
2022-11-18 16:31:35 -0600

    Enable bootsrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//import Rails from "@rails/ujs"
+//import Turbolinks from "turbolinks"
+
+
 import "@hotwired/turbo-rails"
 import "controllers"
-import "bootstrap"
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+require('bootstrap')
+require("@fortawesome/fontawesome-free/js/all")
+
+//import "bootstrap"
+//import "../stylesheets/application"
+
+
+
